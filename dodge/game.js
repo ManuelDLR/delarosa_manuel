f0c7c62c128fc8016e9b95c82a3873fb0178298d
Manuel <01delarosamanuel@gmail.com>
2016-07-21 23:09:44 +0000

    usa

--- dodge/game.js
+++ dodge/game.js
@@ -6,13 +6,13 @@ var backgroundImage;
 var isGameOver;
 
 function preload() {
-    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
-    enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
-    backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png");
+    playerImage = loadImage("obama.png");
+    enemyImage = loadImage("trump.png");
+    backgroundImage = loadImage("usflag.jpg");
 }
 
 function setup() {
-    createCanvas(250, 250);
+    createCanvas(500, 500);
     player = createSprite(width/2, height-(playerImage.height/2), 0, 0);
     player.addImage(playerImage);
     enemy = createSprite(width/2, 0, 0, 0);
@@ -26,14 +26,14 @@ function draw() {
   background(backgroundImage);
 
   if(keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width/2))){
-    player.position.x += 2;
+    player.position.x += 10;
   }
 
   if(keyDown(LEFT_ARROW) && player.position.x > (playerImage.width/2)){
-    player.position.x -= 2;
+    player.position.x -= 10;
   }
 
-  enemy.position.y = enemy.position.y + 3;
+  enemy.position.y = enemy.position.y + 15;
   
   if (enemy.position.y > height) {
       enemy.position.y = 0;
@@ -58,7 +58,7 @@ function gameOver() {
   background(0);
   textAlign(CENTER);
   fill("white");
-  text("Game Over!", width/2, height/2);
+  text("GAME OVER!!! YOU SUCK!!!", width/2, height/2);
   text("Click anywhere to try again", width/2, 3*height/4);
 }
 
