49437462b82a6a23e80b768eecdd12b706e408ce
Manuel <01delarosamanuel@gmail.com>
2016-07-27 19:11:35 +0000

    1

---
+++ collaborative_sketch/main.js
@@ -0,0 +1,12 @@
+
+var pointsData = firebase.database().ref();
+
+var config = {
+    apiKey: "AIxaSyGsAkHke9lXEU_97a8rYpMn7gOH3eWDxrM";
+    authDomain: "collaborative-draw.firebaseapp.com";
+    databaseURL: "https://collaborative-draw.firebaseio.com";
+    storageBucket: "";
+};
+firebase.intializeApp(config);
+
+var pointsData = firebase.database().ref();
