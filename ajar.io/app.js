3063e25d28e00731a303796db261f2063304ec5b
Manuel <01delarosamanuel@gmail.com>
2016-07-29 22:12:20 +0000

    2

--- ajar.io/app.js
+++ ajar.io/app.js
@@ -5,6 +5,12 @@ var y = 250;
 var mouseX = 250;
 var mouseY = 250;
 var velocity = 2;
+var foodPositions = [
+    [30, 20],
+    [400, 90],
+    [60, 317],
+    [300, 268]
+];
 
 function drawUserCell() {
     context.beginPath();
@@ -39,13 +45,24 @@ function calculatePosition() {
 function drawScreen() {
     clearCanvas();
     calculatePosition();
-    drawUserCell();
+    drawCircle(x, y, 40);
+    drawCircle(foodPositions[0][0], foodPositions[0][1], 10);
+    drawCircle(foodPositions[1][0], foodPositions[1][1], 10);
+    drawCircle(foodPositions[2][0], foodPositions[2][1], 10);
+    drawCircle(foodPositions[3][0], foodPositions[3][1], 10);
     setTimeout(drawScreen, 1000 / 60);
 }
 
 function mouseMoved(e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
+}
+
+function drawCircle(circleX, circleY, radius) {
+    context.beginPath();
+    context.arc(circleX, circleY, radius, 0, 2 * 3.14159);
+    context.fillStyle = "cyan";
+    context.fill();
 }
 
 canvas.addEventListener("mousemove", mouseMoved);
