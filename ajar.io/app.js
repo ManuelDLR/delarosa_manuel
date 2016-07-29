a61aa20150e94a406e4cf3b3cfc3053273ad13a2
Manuel <01delarosamanuel@gmail.com>
2016-07-29 21:54:34 +0000

    1

--- ajar.io/app.js
+++ ajar.io/app.js
@@ -2,18 +2,51 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var x = 250;
 var y = 250;
+var mouseX = 250;
+var mouseY = 250;
+var velocity = 2;
 
 function drawUserCell() {
-context.beginPath();
-context.arc(x, y, 40, 0, 2 * 3.14159);
-context.fillStyle = "cyan";
-context.fill();
+    context.beginPath();
+    context.arc(x, y, 40, 0, 2 * 3.14159);
+    context.fillStyle = "cyan";
+    context.fill();
 }
 
-function mouseMoved(mouse) {
-    x = mouse.clientX;
-    y = mousr.clientY;
+function clearCanvas() {
+    context.beginPath();
+    context.rect(0, 0, 500, 500);
+    context.fillStyle = "white";
+    context.fill();
 }
 
-canvas.addEventListener("mouseMove", mouseMoved);
-drawUserCell();
+function calculatePosition() {
+    if (mouseX > x) {
+        x = x + velocity;
+    }
+    else {
+        x = x - velocity;
+    }
+
+    if (mouseY > y) {
+        y = y + velocity;
+    }
+    else {
+        y = y - velocity;
+    }
+}
+
+function drawScreen() {
+    clearCanvas();
+    calculatePosition();
+    drawUserCell();
+    setTimeout(drawScreen, 1000 / 60);
+}
+
+function mouseMoved(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+canvas.addEventListener("mousemove", mouseMoved);
+drawScreen();
